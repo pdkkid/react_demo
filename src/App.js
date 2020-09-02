ac6f36906bff769870e47862cf8323d81862d394
Austin Terry <pdkkid1@gmail.com>
2020-09-02 08:51:29 -0600

    Implemented basic routing, as well as api fetching

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Pages
+import Main from "./pages/main";
+import Admin from "./pages/admin";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: {},
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    fetch("http://localhost:8080/product", {
+      crossDomain: true,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data, isLoading: false }));
+  }
+
+  render() {
+    const { products, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading . . .</p>;
+    }
+
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/" exact>
+              <Main products={products} />
+            </Route>
+            <Route path="/admin">
+              <Admin />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
